e762f3101bef6dcc51958d4b699a04079fb6b415
Sven Patrick Meier <sven.patrick.meier@gmail.com>
2025-09-12 15:48:44 +0200

    feat: add rating comments to products

--- src/products/urls.py
+++ src/products/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 from . import views
 
-urlpatterns=[
+urlpatterns = [
     path('', views.product_list, name='products'),
-    path('categories/<slug:category_slug>',
+    path(
+        'category/<slug:category_slug>/',
         views.product_list,
-        name="products_by_category"
+        name='products_by_category'
     ),
-    path('<slug:category_slug>/<int:product_id>',
+    path(
+        'category/<slug:category_slug>/<int:pk>/',
         views.product_detail,
-        name="product_detail"
-    )
+        name='product_detail'
+    ),
 ]
